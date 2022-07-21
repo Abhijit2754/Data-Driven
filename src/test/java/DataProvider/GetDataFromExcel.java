b53075e73861814130a23d4b065d2355ee84023f
Abhijit2754 <abhijitpaithane93@gmail.com>
2022-07-22 02:11:56 +0530

    Changes added - Excel sheet added

--- src/test/java/DataProvider/GetDataFromExcel.java
+++ src/test/java/DataProvider/GetDataFromExcel.java
@@ -1,5 +1,37 @@
 package DataProvider;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class GetDataFromExcel {
 
+	
+	@DataProvider(name="passdata")
+	public String[][] datapass() throws IOException
+	{
+		File file = new File(System.getProperty("user.dir")+"\\TestData\\Data.xlsx");
+
+		FileInputStream fileIn = new FileInputStream(file);
+		
+		XSSFWorkbook workbook = new XSSFWorkbook(fileIn);
+		int rowsCount = workbook.getSheet("Sheet1").getPhysicalNumberOfRows();
+		int colcount = workbook.getSheet("Sheet1").getRow(0).getPhysicalNumberOfCells();
+		
+		String[][] data = new String[rowsCount][colcount];
+		
+		for(int i = 0; i < data.length;i++)
+		{
+			for(int j = 0 ; j < data[i].length;j++ )
+				data[i][j] = workbook.getSheet("Sheet1").getRow(i).getCell(j).getStringCellValue();
+			
+		}
+		
+		return data;
+	}
 }
--- src/test/java/TestCases/HrmLogin.java
+++ src/test/java/TestCases/HrmLogin.java
@@ -1,12 +1,14 @@
 package TestCases;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import Configuration.BaseClass;
 import ObjectMethods.LoginPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+import DataProvider.*;
 
 public class HrmLogin extends BaseClass  {
 	
@@ -17,38 +19,19 @@ public class HrmLogin extends BaseClass  {
 		getUrl();
 	}
 	
-	@Test(dataProvider = "passdata")	
+	@Test(dataProvider = "passdata", dataProviderClass = GetDataFromExcel.class)	
 	public void Login(String getUsername, String getPassword) 
 	{	
 		LoginPage login = new LoginPage(driver);		
 		login.userName(getUsername);
 		login.password(getPassword);
 		login.clickButton();
-		
-		
+		Assert.assertEquals(driver.getCurrentUrl(),"https://opensource-demo.orangehrmlive.com/index.php/dashboard");
 	}
 	
-	@DataProvider(name="passdata")
-	public String[][] datapass()
+	@AfterMethod
+	public void tearDown()
 	{
-		String[][] data = new String[5][2];
-		
-		data[0][0] = "Admin";
-		data[0][1] = "admin123";
-		
-		data[1][0] = "Admin";
-		data[1][1] = "password";
-		
-		data[2][0] = "Samamsa";
-		data[2][1] = "daasd132";
-		
-		data[3][0] = "Fasdas";
-		data[3][1] = "asdsad";
-		
-		data[4][0] = "SAFFdf";
-		data[4][1] = "cdsdcsdcds";
-		
-		
-		return data;
+		driver.close();
 	}
 }
